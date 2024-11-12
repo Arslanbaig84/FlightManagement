5762007593e1ef4d8db875475a4a456d802a250c
Arslanbaig84 <arslan.multiline@gmail.com>
2024-11-12 21:25:16 +0500

    airport route done

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Airport
 
 # Create your views here.
+def airports(request):
+    airports = Airport.objects.all()
+    return render(request, 'airport/index.html', {'airports':airports})
